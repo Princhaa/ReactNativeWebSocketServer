6614a96869fd53b109620aa9906fec23bd7e1927
Princhaa <deras.hujan15@gmail.com>
2017-04-06 17:00:29 +0700

    Update

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log('Running on port: '+server.address().port)
+})
